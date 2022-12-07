43245fdd1548b7c1e71e95baa23e9ca185e82c1e
LepretreA <alex.lepretre@aol.com>
2022-12-07 15:25:35 +0100

    exo 2 ET 2.2

--- exo2.php
+++ exo2.php
@@ -9,11 +9,48 @@
 </head>
 <body>
     
+<table border="1">
     <?php
 
-
-
+        $array=
+        [
+            0=>5,
+            1=>57,
+            2=>3,
+            3=>800,
+            4=>4,
+        ];
+        echo "<tr>";
+        for ($i=0; $i < sizeof($array) ; $i++) { 
+            echo "<th>".$array[$i]."</th>";
+        }
+        echo "</tr>";
+        
     ?>
+</table>
+<table border="1">
+    <?php
+        $Lucas=
+        [
+            0=>'BURGUET LE SAINT SAUVEUR  #DEUSVULT',
+            1=>'LUCAS LE SAINT DEMI MORT',
+            2=>'654321',
+        ];
+        echo "<tr>";
+        for ($i=0; $i < sizeof($Lucas) ; $i++) { 
+            echo "<th>".$Lucas[$i]."</th>";
+        }
+        echo "</tr>";
+
+        
+    ?>
+    
+
+    
+    
+</table>
+
+
 
 </body>
 </html>
